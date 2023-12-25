2b80b38a58f8d0bfa6f67fcc198d314f0deca478
Amanda Colares <colares.amd@gmail.com>
2023-12-25 10:11:12 -0300

    unit 67 done: sending form with GET method

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>@yield('titulo') | Super Gestão</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    </head>
+
+    <body>
+        @yield('conteudo')
+    </body>
+</html>
