cda4a6dd48d3e331cb22eed658f29d07a7d875f2
Amanda Colares <colares.amd@gmail.com>
2023-12-27 14:10:17 -0300

    to save in another computer

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -7,8 +7,7 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
-        $title = 'Contato';
-        var_dump($_GET);
-        return view('site.contato', compact('title'));
+        var_dump($_POST);
+        return view('site.contato');
     }
 }
